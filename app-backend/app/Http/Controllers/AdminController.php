65ba9da8141ee0e8795e95e9b7ccf6b176c077d6
Ivan-Stoykov <ivanxrist8@gmail.com>
2026-05-02 17:55:51 +0300

    Added fetching for single user

--- app-backend/app/Http/Controllers/AdminController.php
+++ app-backend/app/Http/Controllers/AdminController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 use App\Models\User;
 class AdminController extends Controller
 {
-    public function users()
+    public function usersList()
     {
         $users = User::get();
         return response($users, 200);
+    }
+
+    public function user(string $id)
+    {
+        $user = User::where("id", $id)->first();
+        return response($user, 200);
     }
 }
--- app-backend/routes/api.php
+++ app-backend/routes/api.php
@@ -10,4 +10,5 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('signup', [AuthController::class, 'signup']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::apiResource('phones', PhoneController::class);
-Route::get('/users', [AdminController::class, 'users']);
+Route::get('/users', [AdminController::class, 'usersList']);
+Route::get('/users/{id}', [AdminController::class, 'user']);
